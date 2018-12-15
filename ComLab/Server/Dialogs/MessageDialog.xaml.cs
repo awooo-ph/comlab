c513361ed7d5c7a5d69a24caabf3b07f782cae5f
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-12-16 00:22:26 +0800

    Create MessageDialog.xaml.cs

---
+++ ComLab/Server/Dialogs/MessageDialog.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+using MaterialDesignThemes.Wpf;
+
+namespace ComLab.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for MessageDialog.xaml
+    /// </summary>
+    public partial class MessageDialog
+    {
+        public MessageDialog()
+        {
+            InitializeComponent();
+        }
+
+        public async Task<bool> Show()
+        {
+            var res = await DialogHost.Show(this, "DialogHost").ConfigureAwait(false);
+            if (res == null) return false;
+            return res is bool b && b;
+        }
+
+        public static async Task<bool> Show(string title, string positive, PackIconKind icon = PackIconKind.Alert)
+        {
+            return await Show(title, "", positive);
+        }
+
+        public static async Task<bool> Show(string title, string message, string positive, string negative=null, PackIconKind icon = PackIconKind.Alert)
+        {
+            var dlg = new MessageDialog();
+            dlg.Title.Text = title;
+            dlg.Message.Text = message;
+            dlg.Positive.Content = positive;
+            dlg.Icon.Kind = icon;
+            if (!string.IsNullOrEmpty(negative))
+            {
+                dlg.Negative.Content = negative;
+            }
+            else
+            {
+                dlg.Negative.Visibility = Visibility.Collapsed;
+            }
+
+            return await dlg.Show().ConfigureAwait(false);
+        }
+    }
+}
