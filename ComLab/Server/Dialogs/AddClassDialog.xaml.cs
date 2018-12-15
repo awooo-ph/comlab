82f3191e39ac47584000a2ec12e275844233d70f
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-12-16 00:22:40 +0800

    AddClassDialog

---
+++ ComLab/Server/Dialogs/AddClassDialog.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ComLab.Models;
+using MaterialDesignThemes.Wpf;
+
+namespace ComLab.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for AddClassDialog.xaml
+    /// </summary>
+    public partial class AddClassDialog : UserControl
+    {
+        public AddClassDialog()
+        {
+            InitializeComponent();
+            Class = new Class();
+            DataContext = this;
+        }
+
+        public static async Task<Class> Show()
+        {
+            var dlg = new AddClassDialog();
+            var res = await DialogHost.Show(dlg, "DialogHost") as bool?;
+            if (!(res ?? false)) return null;
+            return dlg.Class;
+        }
+
+        public Class Class { get; set; }
+    }
+}
