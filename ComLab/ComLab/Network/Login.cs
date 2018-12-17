17622e292e85bfd8880f6ea77e92ef7668ada4df
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-12-17 16:37:50 +0800

    login

---
+++ ComLab/ComLab/Network/Login.cs
@@ -0,0 +1,37 @@
+﻿using ProtoBuf;
+
+namespace ComLab.Network
+{
+    [ProtoContract]
+    class Login:Packet<Login>
+    {
+        [ProtoMember(1)]
+        public string Username { get; set; }
+        [ProtoMember(2)]
+        public byte[] Password { get; set; }
+    }
+
+    [ProtoContract]
+    class LoginResult : Packet<LoginResult>
+    {
+        [ProtoMember(1)]
+        public string Message { get; set; }
+        [ProtoMember(2)]
+        public bool Success { get; set; }
+        [ProtoMember(3)]
+        public Student Student { get; set; }
+
+
+    }
+
+    [ProtoContract]
+    class ClassInfo
+    {
+        [ProtoMember(1)]
+        public string ClassName { get; set; }
+        [ProtoMember(2)]
+        public string Schedule { get; set; }
+        [ProtoMember(3)]
+        public string Instructor { get; set; }
+    }
+}
---
+++ ComLab/ComLab/Network/LogonInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ProtoBuf;
+
+namespace ComLab.Network
+{
+    [ProtoContract]
+    class LogonInfo:Packet<LogonInfo>
+    {
+        [ProtoMember(1)]
+        public string IP { get; set; }
+        [ProtoMember(2)]
+        public int Port { get; set; }
+        [ProtoMember(3)] public string ComputerName { get; set; }
+    }
+}
