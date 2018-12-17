3035559c2bfbb636fef58c3884c8828617aae99a
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-12-17 16:37:43 +0800

    Create ServerInfo.cs

---
+++ ComLab/ComLab/Network/ServerInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ProtoBuf;
+
+namespace ComLab.Network
+{
+    [ProtoContract]
+    class ServerInfo:Packet<ServerInfo>
+    {
+        [ProtoMember(1)]
+        public string IP { get; set; }
+        [ProtoMember(2)]
+        public int Port { get; set; }
+        [ProtoMember(3)]
+        public bool ClassStarted { get; set; }
+    }
+}
