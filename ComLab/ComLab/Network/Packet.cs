779eed1e13ee64edc5a9414b78fab88577a9de91
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-12-17 16:37:40 +0800

    Create Packet.cs

---
+++ ComLab/ComLab/Network/Packet.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using ComLab.ViewModels;
+using NetworkCommsDotNet;
+using NetworkCommsDotNet.Connections;
+using NetworkCommsDotNet.Connections.UDP;
+using ProtoBuf;
+
+namespace ComLab.Network
+{
+    [ProtoContract]
+    abstract class Packet<T>:ViewModelBase where T : Packet<T>
+    {
+        public static string Header => typeof(T).Name;
+
+        public override string ToString()
+        {
+            return Header;
+        }
+
+        public async Task Send(IPEndPoint ep)
+        {
+            if (ep == null) return;
+            var sent = false;
+            while (!sent)
+            {
+                try
+                {
+                    UDPConnection.SendObject(Header, this, ep, NetworkComms.DefaultSendReceiveOptions,
+                        ApplicationLayerProtocolStatus.Enabled);
+                    sent = true;
+                    break;
+                }
+                catch (Exception)
+                {
+                    await Task.Delay(100);
+                }
+            }
+        }
+
+    }
+}
