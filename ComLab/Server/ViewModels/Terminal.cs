5c94f066f58256a8fc5cd840f73a55f664abce81
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-12-17 16:37:19 +0800

    Terminals

---
+++ ComLab/Server/ViewModels/Terminal.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComLab.ViewModels
+{
+    class Terminal:ViewModelBase
+    {
+        private string _Name;
+
+        public string Name
+        {
+            get => _Name;
+            set
+            {
+                if (value == _Name) return;
+                _Name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        private string _IP;
+
+        public string IP
+        {
+            get => _IP;
+            set
+            {
+                if (value == _IP) return;
+                _IP = value;
+                OnPropertyChanged(nameof(IP));
+            }
+        }
+
+        private Students _User;
+
+        public Students User
+        {
+            get => _User;
+            set
+            {
+                if (value == _User) return;
+                _User = value;
+                OnPropertyChanged(nameof(User));
+            }
+        }
+
+
+    }
+}
---
+++ ComLab/Server/ViewModels/Terminals.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComLab.ViewModels
+{
+    class Terminals:ViewModelBase
+    {
+        private Terminals() { }
+
+        private static Terminals _instance;
+        public static Terminals Instance => _instance ?? (_instance = new Terminals());
+    }
+}
