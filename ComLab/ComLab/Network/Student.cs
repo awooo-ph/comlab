35783517c1326e2aaf18e5adbe37c3d11d95ff4f
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-12-17 16:37:36 +0800

    Create Student.cs

---
+++ ComLab/ComLab/Network/Student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ProtoBuf;
+
+namespace ComLab.Network
+{
+    [ProtoContract]
+    class Student:Packet<Student>
+    {
+        public Student() { }
+
+        private string _Firstname;
+
+        public string Firstname
+        {
+            get => _Firstname;
+            set
+            {
+                if (value == _Firstname) return;
+                _Firstname = value;
+                OnPropertyChanged(nameof(Firstname));
+            }
+        }
+
+        private string _Lastname;
+
+        public string Lastname
+        {
+            get => _Lastname;
+            set
+            {
+                if (value == _Lastname) return;
+                _Lastname = value;
+                OnPropertyChanged(nameof(Lastname));
+            }
+        }
+
+        private string _Course;
+
+        public string Course
+        {
+            get => _Course;
+            set
+            {
+                if (value == _Course) return;
+                _Course = value;
+                OnPropertyChanged(nameof(Course));
+            }
+        }
+
+
+    }
+}
