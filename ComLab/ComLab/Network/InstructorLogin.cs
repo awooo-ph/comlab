7ce89c9f69d30a826c2b21aaca0aea5489053e67
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-12-20 19:29:37 +0800

    Create InstructorLogin.cs

---
+++ ComLab/ComLab/Network/InstructorLogin.cs
@@ -0,0 +1,18 @@
+﻿using ProtoBuf;
+
+namespace ComLab.Network
+{
+    [ProtoContract]
+    class InstructorLogin : Packet<InstructorLogin>
+    {
+        [ProtoMember(1)]
+        public string Fullname { get; set; }
+    }
+
+    [ProtoContract]
+    class InstructorLogout : Packet<InstructorLogout>
+    {
+        [ProtoMember(1)]
+        public bool Lock { get; set; } = true;
+    }
+}
